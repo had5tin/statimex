6204ab30a6b409b66bec9a0d1f881ab773708749
halv2 <halv2@dkt.com.vn>
2017-04-04 01:11:55 +0700

    update

--- template-parts/tin-tratimex.php
+++ template-parts/tin-tratimex.php
@@ -8,34 +8,196 @@ $news = new WP_Query([
 ]);
 ?>
 <div id="tin-tuc" class="branch cover container" >
-    <div class="row">
-        <div class="col-md-3">
-            <div class="title">
-                <h2>Tin tức</h2>
-                <hr>
-            </div>
-            <div class="sidebar-left">
-                <ul class="item-list">
-                    <li class="item">
-                        <a href="./tin-trong-nghanh/" class="active">Tin trong ngành</a>
-                    </li>
-                    <li class="item active">
-                        <a href="./tin-tuc">Tin Statimex</a>
-                    <li class="item ">
-                        <a href="./tin-tuc-2/">Tất cả tin tức</a>
-                    </li>
-                </ul>
+    <div class="l-container">
+        <div class="l-hero hero  hero--small-heading ">
+            <div class="l-hero__text">
+                <div class="hero__title">
+                    <h1 class="t-heading-section hero__heading">
+                        Tin tức và Sự Kiện
+                    </h1>
+                </div>
             </div>
         </div>
-        <div class="col-md-9">
-            <div class="product-list featured-news animatedParent">
-                <?php
-                while ($news->have_posts()) {
-                    $news->the_post();
-                    get_template_part('inc/tin-tuc/single');
-                }
-                ?>
+        <div class="l-main-w-sidebar">
+            <div class="l-main-w-sidebar__main">
+
+                <div class="js-ajax-entries ajax-grid">
+
+                    <div class="js-entry-grid ">
+
+                        <div class="l-entry-grid js-entries">
+                            <div class="l-media-block l-media-block--2-up h-animation-pop-up">
+                                <a href="http://tratimex.crystal-techs.com/vi/bat-nhao-thi-truong-nhua-duong/"
+                                   title="Hà Nội: Cầu đi bộ chảy nhựa vì nắng nóng"
+                                   class="media-block">
+                                    <div class="media-block__img lazyloaded" data-bgset=""
+                                         style="background-image: url('http://tratimex.crystal-techs.com/wp-content/uploads/2017/02/35-FILEminimizer.jpg');">
+                                    </div>
+                                    <div class="media-block__text">
+                                        <h5 class="media-block__title">
+                                            Bát nháo thị trường nhựa đường
+                                        </h5>
+                                        <h6 class="media-block__sub-title">
+                                            Thời gian qua có thông tin về tình trạng một...
+                                        </h6>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="l-media-block l-media-block--2-up h-animation-pop-up">
+                                <a href="http://tratimex.crystal-techs.com/vi/bat-nhao-thi-truong-nhua-duong/"
+                                   title="Hà Nội: Cầu đi bộ chảy nhựa vì nắng nóng"
+                                   class="media-block">
+                                    <div class="media-block__img lazyloaded" data-bgset=""
+                                         style="background-image: url('http://tratimex.crystal-techs.com/wp-content/uploads/2017/02/35-FILEminimizer.jpg');">
+                                    </div>
+                                    <div class="media-block__text">
+                                        <h5 class="media-block__title">
+                                            Bát nháo thị trường nhựa đường
+                                        </h5>
+                                        <h6 class="media-block__sub-title">
+                                            Thời gian qua có thông tin về tình trạng một...
+                                        </h6>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="l-media-block l-media-block--2-up h-animation-pop-up">
+                                <a href="http://tratimex.crystal-techs.com/vi/bat-nhao-thi-truong-nhua-duong/"
+                                   title="Hà Nội: Cầu đi bộ chảy nhựa vì nắng nóng"
+                                   class="media-block">
+                                    <div class="media-block__img lazyloaded" data-bgset=""
+                                         style="background-image: url('http://tratimex.crystal-techs.com/wp-content/uploads/2017/02/35-FILEminimizer.jpg');">
+                                    </div>
+                                    <div class="media-block__text">
+                                        <h5 class="media-block__title">
+                                            Bát nháo thị trường nhựa đường
+                                        </h5>
+                                        <h6 class="media-block__sub-title">
+                                            Thời gian qua có thông tin về tình trạng một...
+                                        </h6>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="l-media-block l-media-block--2-up h-animation-pop-up">
+                                <a href="http://tratimex.crystal-techs.com/vi/bat-nhao-thi-truong-nhua-duong/"
+                                   title="Hà Nội: Cầu đi bộ chảy nhựa vì nắng nóng"
+                                   class="media-block">
+                                    <div class="media-block__img lazyloaded" data-bgset=""
+                                         style="background-image: url('http://tratimex.crystal-techs.com/wp-content/uploads/2017/02/35-FILEminimizer.jpg');">
+                                    </div>
+                                    <div class="media-block__text">
+                                        <h5 class="media-block__title">
+                                            Bát nháo thị trường nhựa đường
+                                        </h5>
+                                        <h6 class="media-block__sub-title">
+                                            Thời gian qua có thông tin về tình trạng một...
+                                        </h6>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
             </div>
+
+
+            <div class="l-main-w-sidebar__sidebar h-animation-pop-up">
+
+
+                <div class="blog-home-sidebar">
+
+                    <h3 class="blog-home-sidebar__heading t-eyebrow-a h-color-text-gold">
+                        Sự kiện sắp tới
+                    </h3>
+
+                    <div class="l-media-block ">
+                        <a href="http://tratimex.crystal-techs.com/vi/mang-nhua-duong-gap-kho-khan/"
+                           title="Mảng nhựa đường gặp khó khăn, PLC điều chỉnh kế hoạch SXKD năm 2016"
+                           class="media-block media-block--mini">
+
+                            <div class="media-block__img lazyloaded"
+                                 style="background-image: url('http://tratimex.crystal-techs.com/wp-content/uploads/2017/02/B66A9548-FILEminimizer.jpg');">
+                            </div>
+
+                            <div class="media-block__text">
+                                <h5 class="media-block__title">
+                                    Mảng nhựa đường gặp khó khăn, PLC...
+                                </h5>
+                                <h6 class="media-block__sub-title">
+                                    Mảng nhựa đường gặp khó khăn, PLC điều chỉnh giảm...
+                                </h6>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="l-media-block ">
+                        <a href="http://tratimex.crystal-techs.com/vi/mang-nhua-duong-gap-kho-khan/"
+                           title="Mảng nhựa đường gặp khó khăn, PLC điều chỉnh kế hoạch SXKD năm 2016"
+                           class="media-block media-block--mini">
+
+                            <div class="media-block__img lazyloaded"
+                                 style="background-image: url('http://tratimex.crystal-techs.com/wp-content/uploads/2017/02/B66A9548-FILEminimizer.jpg');">
+                            </div>
+
+                            <div class="media-block__text">
+                                <h5 class="media-block__title">
+                                    Mảng nhựa đường gặp khó khăn, PLC...
+                                </h5>
+                                <h6 class="media-block__sub-title">
+                                    Mảng nhựa đường gặp khó khăn, PLC điều chỉnh giảm...
+                                </h6>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="l-section l-section--small">
+                        <h3 class="blog-home-sidebar__heading t-eyebrow-a h-color-text-gold">
+                            Sự kiện đã qua
+                        </h3>
+
+                        <div class="l-media-block ">
+                            <a href="http://tratimex.crystal-techs.com/vi/mang-nhua-duong-gap-kho-khan/"
+                               title="Mảng nhựa đường gặp khó khăn, PLC điều chỉnh kế hoạch SXKD năm 2016"
+                               class="media-block media-block--mini">
+
+                                <div class="media-block__img lazyloaded"
+                                     style="background-image: url('http://tratimex.crystal-techs.com/wp-content/uploads/2017/02/B66A9548-FILEminimizer.jpg');">
+                                </div>
+
+                                <div class="media-block__text">
+                                    <h5 class="media-block__title">
+                                        Mảng nhựa đường gặp khó khăn, PLC...
+                                    </h5>
+                                    <h6 class="media-block__sub-title">
+                                        Mảng nhựa đường gặp khó khăn, PLC điều chỉnh giảm...
+                                    </h6>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="l-media-block ">
+                            <a href="http://tratimex.crystal-techs.com/vi/mang-nhua-duong-gap-kho-khan/"
+                               title="Mảng nhựa đường gặp khó khăn, PLC điều chỉnh kế hoạch SXKD năm 2016"
+                               class="media-block media-block--mini">
+
+                                <div class="media-block__img lazyloaded"
+                                     style="background-image: url('http://tratimex.crystal-techs.com/wp-content/uploads/2017/02/B66A9548-FILEminimizer.jpg');">
+                                </div>
+
+                                <div class="media-block__text">
+                                    <h5 class="media-block__title">
+                                        Mảng nhựa đường gặp khó khăn, PLC...
+                                    </h5>
+                                    <h6 class="media-block__sub-title">
+                                        Mảng nhựa đường gặp khó khăn, PLC điều chỉnh giảm...
+                                    </h6>
+                                </div>
+                            </a>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
